87a8fb3538f1b0f379dcd78c76fa00c8c977c334
Alex Júnior <alexjunior651@hotmail.com>
2022-05-01 01:43:24 -0300

    Adição do carrosel com jquary do fontawesome, correção do background, e Media Queries no CSS

---
+++ js/olw/setup.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+// evento passar mouseover
+var title = document.querySelector(".bootes")
+title.addEventListener("mouseover", function () {
+
+});
+
+// evento passar mouseout
+title.addEventListener("mouseout", function () {
+
+});
